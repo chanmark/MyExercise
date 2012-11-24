b732e41cd77e48fb05b28877ec7a29c8c4475e5f
mark <jhengbangmark@gmail.com>
2012-11-24 13:48:17 +0800

    Use wait queue do process scheduling

--- cdata.c
+++ cdata.c
@@ -6,6 +6,8 @@
 #include <linux/miscdevice.h>
 #include <linux/wait.h>
 #include <asm/io.h>
+#include <linux/ioctl.h>
+#include "cdata_ioctl.h"
 #include <asm/uaccess.h>
 
 #ifdef CONFIG_SMP
@@ -13,21 +15,28 @@
 #endif
 
 #define	CDATA_MAJOR 121 
-#define BUFSIZE     1024
+#define     BUFSIZE	1024
 
 struct cdata_t {
-    char        data[BUFSIZE];
-    int         index;
+	char data[BUFSIZE];
+	int  index;
+
+	wait_queue_head_t wait;
 };
+
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-    struct cdata_t *cdata;
-
-    cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
-    cdata->index = 0;
-
-    filp->private_data = (void *)cdata;
+	int minor;
+	struct cdata_t *cdata;
+	minor = MINOR(inode->i_rdev);
+	printk(KERN_ALERT "filp Address = %p\n", filp);
+	
+	cdata->index = 0;
+	
+	cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t),GFP_KERNEL);
+	init_waitqueue_head(&cdata->wait);
+	filp->private_data = (void *)cdata; 
 
 	return 0;
 }
@@ -35,9 +44,24 @@ static int cdata_open(struct inode *inode, struct file *filp)
 static int cdata_ioctl(struct inode *inode, struct file *filp, 
 			unsigned int cmd, unsigned long arg)
 {
-	printk(KERN_ALERT "cdata: in cdata_ioctl()\n");
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
 
-    return 0;
+	switch(cmd)
+	{
+		case IOCTL_EMPTY:
+			printk(KERN_ALERT "in ioctl: IOCTL_EMPTY");
+			cdata->data[0] = '\0';
+			cdata->index = 0;
+			break;
+		case IOCTL_SYNC:
+			printk(KERN_ALERT "Stream = %s",cdata->data);
+			break;
+		default:
+			return EINVAL;
+	}
+			
+	printk(KERN_ALERT "cdata: in cdata_ioctl()\n");
+	return 0;
 }
 
 static ssize_t cdata_read(struct file *filp, char *buf, 
@@ -49,27 +73,37 @@ static ssize_t cdata_read(struct file *filp, char *buf,
 static ssize_t cdata_write(struct file *filp, const char *buf, 
 				size_t count, loff_t *off)
 {
-    struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
-    int i;
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	DECLARE_WAITQUEUE(wait, current);
+	int i;
+	//mutex lock
+	for( i=0; i < count; i++){
+		if( cdata->index >= BUFSIZE){
+			add_wait_queue(&cdata->wait, &wait);
+			set_current_state(TASK_INTERRUPTIBLE);
+			//current->state = TASK_UNINTERRUPTIBLE;
+			schedule();
+			//current->state = TASK_RUNNING; -> it is Error;
+			current->state = TASK_RUNNING;
+			remove_wait_queue(&cdata->wait, &wait);
+		}
 
-    for (i = 0; i < count; i++) {
-        if (cdata->index >= BUFSIZE)
-            return -EFAULT;
-
-        if (copy_from_user(&cdata->data[cdata->index++], &buf[i], 1))
-            return -EFAULT;
-    }
-
+		if( copy_from_user(&cdata->data[cdata->index++], &buf[i], 1) )
+			return -EFAULT;
+	}
+	// mutex unlock
 	return 0;
 }
 
 static int cdata_release(struct inode *inode, struct file *filp)
 {
 	printk(KERN_ALERT "cdata: in cdata_release()\n");
+	kfree(filp->private_data);
 	return 0;
 }
 
 struct file_operations cdata_fops = {	
+	owner:		THIS_MODULE,
 	open:		cdata_open,
 	release:	cdata_release,
 	ioctl:		cdata_ioctl,
